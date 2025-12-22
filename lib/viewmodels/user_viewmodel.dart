a139501f5506a836c6791ca30c10f19d1b255287
sovannvath <sovannvath69@gmail.com>
2025-12-23 00:41:18 +0700

    authentication

--- lib/viewmodels/user_viewmodel.dart
+++ lib/viewmodels/user_viewmodel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class UserViewModel extends ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -15,9 +16,32 @@ class UserViewModel extends ChangeNotifier {
     notifyListeners();
   }
 
+  Future<void> signInWithGoogle() async {
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+    final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+
+    if (googleUser == null) return; // user cancelled
+
+    final GoogleSignInAuthentication googleAuth =
+    await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final UserCredential result =
+    await _auth.signInWithCredential(credential);
+
+    user = result.user;
+    notifyListeners();
+  }
+
   Future<void> signOut() async {
     await _auth.signOut();
     user = null;
     notifyListeners();
   }
 }
+
+
